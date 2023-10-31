6ebb128e27076ee523ff59e4e8636461d9685d78
Charlie Nguyen <vawntanng@gmail.com>
2023-10-31 08:41:48 +0700

    [CHANGE] change UI doctor list page

--- src/pages/doctor-list/DoctorList.jsx
+++ src/pages/doctor-list/DoctorList.jsx
@@ -1,17 +1,9 @@
 import Box from "@mui/material/Box";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import Header from "../../component/header/Header.jsx";
-import { Container, Typography } from "@mui/material";
+import {Container, FormControl, InputLabel, MenuItem, Select, Typography} from "@mui/material";
 import Footer from "../../component/Footer/Footer.jsx";
-import { getValueAPI } from "../../../api-service.js";
-import Paper from "@mui/material/Paper";
-import TableContainer from "@mui/material/TableContainer";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
-import TableBody from "@mui/material/TableBody";
-import TablePagination from "@mui/material/TablePagination";
+import {getValueAPI} from "../../../api-service.js";
 
 
 const columns = [
@@ -39,12 +31,12 @@ const columns = [
         id: 'specName',
         label: 'Spec name',
         minWidth: 200,
-    } ];
+    }];
 const DoctorList = () => {
-    const [ pecialization, setSecialization ] = useState([])
-    const [ page, setPage ] = useState(0);
-    const [ rowsPerPage, setRowsPerPage ] = useState(10);
-    const [ users, setUsers ] = useState([])
+    const [pecialization, setSecialization] = useState([])
+    const [percializationChoose, setPercializationChoose] = useState("")
+    const [listDoctors, setListDoctors] = useState([])
+    const [currentDoctor, setCurrentDoctor] = useState(null)
     const getData = async () => {
         const specializations = await getValueAPI("/api/specialization/getAll")
         setSecialization(specializations)
@@ -53,132 +45,174 @@ const DoctorList = () => {
         getData()
         return getData;
     }, []);
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+    const handleChangePecialization = async (event) => {
+        const data = await getValueAPI(`/api/doctor/getBySpecId/${event.target.value}`)
+        setListDoctors(data)
+        console.log('data', data)
+        setPercializationChoose(event.target.value)
     };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-    const handleGetId = async (id) => {
-        const data = await getValueAPI(`/api/doctor/getBySpecId/${ id }`)
-        setUsers(data)
+
+    const handleChooseDoctor = async (e) => {
+        const findDoctor = listDoctors.find(doctor => doctor.id === e.target.value)
+        setCurrentDoctor(findDoctor)
     }
-    return (<Box sx={ {
+    return (<Box sx={{
         display: "flex",
         flexDirection: "column",
         height: "100vh",
-    } }>
+    }}>
         <Box sx={{
             height: "80px",
         }}><Header/></Box>
-        <Box sx={ {
+        <Box sx={{
             flex: "1 1 0",
-        } }>
+        }}>
             <Container>
-                <Box sx={ {
-                    marginTop: "20px",
-                } }>
-                    <Box sx={ {
-                        height: "1px",
-                        width: "100%",
-                        backgroundColor: "#000",
-                    } }/>
-                    <Paper sx={ {
-                        width: '100%',
-                        overflow: 'hidden'
-                    } }>
-                        <TableContainer component={ Paper } sx={ { height: 440 } }>
-                            <Table sx={ {
-                                minWidth: 200,
-                                color: "black"
-                            } } stickyHeader aria-label="sticky table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell sx={ {
-                                            color: "white",
-                                            background: "#383838",
-                                            fontSize: "18px"
-                                        } }>Faculty name</TableCell>
-                                        <TableCell sx={ {
-                                            color: "white",
-                                            background: "#383838",
-                                            fontSize: "18px"
-                                        } } align="right">ID</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    { pecialization.map((row) => (
-                                        <TableRow
-                                            key={ row.id }
-                                            sx={ { '&:last-child td, &:last-child th': { border: 0 } } }
-                                            onClick={ () => handleGetId(row.id) }
-                                        >
-                                            <TableCell component="th" scope="row">
-                                                { row.specName }
-                                            </TableCell>
-                                            <TableCell align="right">{ row.id }</TableCell>
-                                        </TableRow>
-                                    )) }
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Paper>
-                    <Box sx={ {
-                        height: "1px",
-                        width: "100%",
-                        backgroundColor: "#000",
-                        margin: "40px 0"
-                    } }/>
+                <Box sx={{
+                    width: "100%",
+                    padding: "10px 20px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}>
+                    <FormControl sx={{width: "45%"}}>
+                        <InputLabel id="demo-simple-select-label">Faculty list</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Danh Sách Khoa"
+                            defaultValue=""
+                            name="departmentId"
+                            onChange={handleChangePecialization}
+                        >
+                            {pecialization.map((value) => <MenuItem key={value.id}
+                                                                    value={value.id}>{value.specName}</MenuItem>)}
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{width: "45%"}}>
+                        <InputLabel id="demo-simple-select-label">Specialized doctor</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="specialized doctor"
+                            name="id"
+                            defaultValue=""
+                            onChange={handleChooseDoctor}
+                        >
+                            {listDoctors.map((value) => <MenuItem key={value.id}
+                                                                  value={value.id}>{value.fullName}</MenuItem>)}
+                        </Select>
+                    </FormControl>
                 </Box>
-                <Paper sx={ {
-                    width: '100%',
-                    overflow: 'hidden'
-                } }>
-                    <TableContainer sx={ { maxHeight: 440 } }>
-                        <Table stickyHeader aria-label="sticky table">
-                            <TableHead sx={ { backgroundColor: "#363432" } }>
-                                <TableRow>
-                                    { columns.map((column, index) => (<TableCell
-                                        key={ index }
-                                        align={ column.align }
-                                        style={ {
-                                            minWidth: column.minWidth,
-                                            backgroundColor: "#363432",
-                                            color: "#fff"
-                                        } }
-                                    >
-                                        { column.label }
-                                    </TableCell>)) }
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                { users
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((row, index) => {
-                                        return (<TableRow hover role="checkbox" tabIndex={ -1 } key={ index }>
-                                            { columns.map((column, index) => {
-                                                const value = row[column.id];
-                                                return (<TableCell key={ index } align={ column.align }>
-                                                    { column.format && typeof value === 'number' ? column.format(value) : value }
-                                                </TableCell>);
-                                            }) }
-                                        </TableRow>);
-                                    }) }
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        rowsPerPageOptions={ [ 10, 25, 100 ] }
-                        component="div"
-                        count={ users.length }
-                        rowsPerPage={ rowsPerPage }
-                        page={ page }
-                        onPageChange={ handleChangePage }
-                        onRowsPerPageChange={ handleChangeRowsPerPage }
-                    />
-                </Paper>
+                <hr/>
+                {currentDoctor && (
+                    <Box sx={{
+                        height: "500px",
+                        width: "100%",
+                        display: "flex",
+                        columnGap: "10px",
+                        borderRadius: "10px",
+                        border: "1px solid #000",
+                        marginTop: "20px",
+                    }}>
+                        <Box sx={{
+                            height: "calc(100% - 40px)",
+                            width: "400px",
+                            backgroundColor: "#e8e8e8",
+                            margin: "20px 10px 20px 20px",
+                            borderRadius: "10px",
+                        }}>
+                            <img style={{
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "cover",
+                                borderRadius: "none"
+                            }}
+                                 src={currentDoctor.image?currentDoctor.image : "https://img.freepik.com/free-vector/hand-drawn-doctor-answer-questions-clipart-gesture-character_40876-3115.jpg?w=1060&t=st=1698716461~exp=1698717061~hmac=92991017ec73bb3dab924043df24e9c930947a55cb9fc48ffa6a677ec5f0d116"}
+                                 alt="avatar-doctor"></img>
+                        </Box>
+                        <Box sx={{
+                            height: "calc(100% - 40px)",
+                            width: "calc(100% - 400px)",
+                            // backgroundColor: "#fff9f4",
+                            margin: "20px 20px 20px 10px",
+                            borderRadius: "10px",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-evenly",
+                        }}>
+                            <Box sx={{
+                                display: "flex",
+                                columnGap: "10px",
+                                margin: "10px 20px",
+                                height: "40px",
+                            }}>
+                                <Typography sx={{
+                                    width: "48%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Full name: {currentDoctor.fullName}</Typography>
+                                <Typography sx={{
+                                    width: "48%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Phone: {currentDoctor.phoneNumber}</Typography>
+                            </Box>
+                            <Box sx={{
+                                display: "flex",
+                                columnGap: "10px",
+                                margin: "10px 20px",
+                                height: "40px",
+                            }}>
+                                <Typography sx={{
+                                    width: "48%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Gender:</Typography>
+                                <Typography sx={{
+                                    width: "48%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Age: {currentDoctor.age}</Typography>
+                            </Box>
+                            <Box sx={{
+                                display: "flex",
+                                columnGap: "10px",
+                                margin: "10px 20px",
+                                height: "40px",
+                            }}>
+                                <Typography sx={{
+                                    width: "100%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Spec name: {currentDoctor.specName}</Typography>
+                            </Box>
+                            <Box sx={{
+                                display: "flex",
+                                columnGap: "10px",
+                                margin: "10px 20px",
+                                height: "40px",
+                            }}>
+                                <Typography sx={{
+                                    width: "100%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Work experience: {currentDoctor.workExperience}</Typography>
+                            </Box>
+                            <Box sx={{
+                                display: "flex",
+                                columnGap: "10px",
+                                margin: "10px 20px",
+                                height: "40px",
+                            }}>
+                                <Typography sx={{
+                                    width: "100%",
+                                    fontSize: "20px",
+                                    fontWeight: "600",
+                                }}>Graduate at: {currentDoctor.graduateAt}</Typography>
+                            </Box>
+                        </Box>
+                    </Box>)}
             </Container>
         </Box>
         <Footer/>
