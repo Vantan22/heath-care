a8e52a7928d86d407ef54d57a6f1b83e82f0c314
Charlie Nguyen <vawntanng@gmail.com>
2023-11-01 00:39:48 +0700

    [ADD] - header

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,22 +1,111 @@
-import { Box, Container } from "@mui/material";
+import { Avatar, Box, Container, Menu, MenuItem, Stack, Typography } from "@mui/material";
 import NavTabs from "./Navigation/NavigationMenu.jsx";
 import { Outlet } from "react-router-dom";
+import Header from "../../component/header/Header.jsx";
+import { useEffect, useState } from "react";
+import PopupState, { bindMenu, bindTrigger } from "material-ui-popup-state";
+import axios from "axios";
 
 const Admin = () => {
+    const [dataUser, setDataUser] = useState({});
+    const currentName = localStorage.getItem('fullName')
+    const patientId = localStorage.getItem("id");
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isDoctor, setIsDoctor] = useState(false);
+
+    const handleLogout = () => {
+        localStorage.removeItem('username')
+        localStorage.removeItem('id')
+        localStorage.removeItem('fullName')
+        navigate('/auth/login')
+    }
+    useEffect(() => {
+        // Gọi API khi component được mount lần đầu
+        axios
+            .get(
+                `https://truculent-kick-production.up.railway.app/api/user/getByUserId/${patientId}`
+            )
+            .then((response) => {
+                setDataUser(response.data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
     return (
         <Box sx={{
-            width: "120px",
+            width: "140px",
             backgroundColor: "#363432",
             minHeight: "100vh",
-            maxWidth: "140px",
-            marginLeft:"-24px"
+            maxWidth: "150px",
         }}>
-            <Container>
+                <Box sx={{
+                    height: '80px',
+                }}>
+                    <header style={{
+                        width: "calc(100% - 140px)",
+                        backgroundColor: "#666666",
+                    position: "fixed",
+                        marginLeft:"140px",
+                        zIndex: "10"
+                    }}>
+                        <Container>
+                            <Box sx={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                                height: "80px",
+                                fixed: "top",
+                            }}>
+                                {currentName ? <Box sx={{
+                                    minWidth: "70px",
+                                    position: "absolute",
+                                    right:"5%"
+                                }}>
+                                    <PopupState variant="popover" popupId="demo-popup-menu">
+                                        {(popupState) => (<>
+                                            <Typography sx={{
+                                                color: "#EBE4D1",
+                                                cursor: "pointer",
+                                            }} variant="contained" {...bindTrigger(popupState)}>
+                                                <Stack direction="row" spacing={2} alignItems="center">
+                                                    <Avatar alt="Remy Sharp" sizes="20"
+                                                        src={dataUser.image ? dataUser.image : "/static/images/avatar/1.jpg"} />
+                                                    <Typography sx={{
+                                                        fontSize: "18px",
+                                                        fontWeight: "500",
+                                                    }}>{dataUser.fullName}</Typography>
+                                                </Stack>
+                                            </Typography>
+                                            <Menu {...bindMenu(popupState)} >
+                                                <MenuItem sx={{
+                                                    width: "150px",
+                                                }} onClick={() =>
+                                                    navigate('/profile')
+                                                }>Profile</MenuItem>
+                                                {isAdmin && <MenuItem
+                                                    onClick={() => navigate('/admin/faculty-management')}>Admin</MenuItem>}
+                                                {isDoctor && <MenuItem
+                                                    onClick={() => navigate('/doctor/create-medical-records')}>Doctor</MenuItem>}
+                                                <MenuItem onClick={() => navigate('/appointment-list')}>Lịch Hẹn</MenuItem>
+                                                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                                            </Menu>
+                                        </>)}
+                                    </PopupState>
+                                </Box> : <Link href="/auth/login" sx={{
+                                    fontSize: "18px",
+                                    fontWeight: "500",
+                                    color: "#fff",
+                                    cursor: "pointer",
+                                }}>Login</Link>}
+                            </Box>
+                        </Container>
+                    </header>
+                </Box>
                 <Box sx={{
                     display: "flex",
                 }}>
                     <Box sx={{
-                        width: "120px",
+                        width: "140px",
                         backgroundColor: "#363432",
                         minHeight: "100vh",
                         maxWidth: "140px",
@@ -30,7 +119,6 @@ const Admin = () => {
                         marginLeft:"120px"
                     }}><Outlet /></Box>
                 </Box>
-            </Container>
         </Box>
         // </Box >
     )
--- src/pages/doctor/Doctor.jsx
+++ src/pages/doctor/Doctor.jsx
@@ -1,16 +1,103 @@
-import { Box, Container } from "@mui/material";
+import { Avatar, Box, Container, Menu, MenuItem, Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import NavTabsForDoctor from "./component/NavigationMenuFormDoctor.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PopupState, { bindMenu, bindTrigger } from "material-ui-popup-state";
+import { Typography } from "antd";
 
 const Doctor = () => {
+    const [dataUser, setDataUser] = useState({});
+    const currentName = localStorage.getItem('fullName')
+    const patientId = localStorage.getItem("id");
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isDoctor, setIsDoctor] = useState(false);
+
+    const handleLogout = () => {
+        localStorage.removeItem('username')
+        localStorage.removeItem('id')
+        localStorage.removeItem('fullName')
+        navigate('/auth/login')
+    }
+    useEffect(() => {
+        // Gọi API khi component được mount lần đầu
+        axios
+            .get(
+                `https://truculent-kick-production.up.railway.app/api/user/getByUserId/${patientId}`
+            )
+            .then((response) => {
+                setDataUser(response.data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
     return (
         <Box sx={{
-            width: "120px",
+            width: "140px",
             backgroundColor: "#363432",
             minHeight: "100vh",
-            maxWidth: "140px",
-            marginLeft:"-24px"
+            maxWidth: "150px",
         }}>
+            <Box sx={{
+                height: '80px',
+            }}>
+                <header style={{
+                    width: "calc(100% - 140px)",
+                    backgroundColor: "#666666",
+                    position: "fixed",
+                    marginLeft: "140px",
+                    zIndex: "10"
+                }}>
+                        <Box sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            height: "80px",
+                            fixed: "top",
+                        }}>
+                            {currentName ? <Box sx={{
+                                minWidth: "70px",
+                                position: "absolute",
+                                right: "5%"
+                            }}>
+                                <PopupState variant="popover" popupId="demo-popup-menu">
+                                    {(popupState) => (<>
+                                        <Typography sx={{
+                                            color: "#EBE4D1",
+                                            cursor: "pointer",
+                                        }} variant="contained" {...bindTrigger(popupState)}>
+                                            <Stack direction="row" spacing={2} alignItems="center">
+                                                <Avatar alt="Remy Sharp" sizes="20"
+                                                    src={dataUser.image ? dataUser.image : "/static/images/avatar/1.jpg"} />
+                                                <Typography sx={{
+                                                    fontSize: "18px",
+                                                    fontWeight: "500",
+                                                }}>{dataUser.fullName}</Typography>
+                                            </Stack>
+                                        </Typography>
+                                        <Menu {...bindMenu(popupState)} >
+                                            <MenuItem sx={{
+                                                width: "150px",
+                                            }} onClick={() =>
+                                                navigate('/profile')
+                                            }>Profile</MenuItem>
+                                            {isAdmin && <MenuItem
+                                                onClick={() => navigate('/admin/faculty-management')}>Admin</MenuItem>}
+                                            {isDoctor && <MenuItem
+                                                onClick={() => navigate('/doctor/create-medical-records')}>Doctor</MenuItem>}
+                                            <MenuItem onClick={() => navigate('/appointment-list')}>Lịch Hẹn</MenuItem>
+                                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                                        </Menu>
+                                    </>)}
+                                </PopupState>
+                            </Box> : <Link href="/auth/login" sx={{
+                                fontSize: "18px",
+                                fontWeight: "500",
+                                color: "#fff",
+                                cursor: "pointer",
+                            }}>Login</Link>}
+                        </Box>
+                </header>
+            </Box>
             <Container>
                 <Box sx={{
                     display: "flex",
